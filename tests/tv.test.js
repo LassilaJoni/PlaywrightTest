1cf4fe933330ff605d27f3a23adabc25d0f1cef5
LassilaJoni <joni@jonilassila.com>
2023-04-11 12:09:04 +0300

    Updated test.

--- tests/tv.test.js
+++ tests/tv.test.js
@@ -1,14 +1,10 @@
 const { test, expect } = require('@playwright/test');
 
-test('YLE TV main page loads', async ({ page }) => {
-  await page.goto('https://areena.yle.fi/tv');
-  const pageTitle = await page.title();
-  expect(pageTitle).toContain('Yle Areena – Enemmän kuin ehdit katsoa ja kuunnella | TV | Areena | yle.fi');
-});
 
 test('Error for providing email in wrong format', async ({ page}) => {
     await page.goto('https://areena.yle.fi/tv');
-
+    const pageTitle = await page.title();
+    expect(pageTitle).toContain('Yle Areena – Enemmän kuin ehdit katsoa ja kuunnella | TV | Areena | yle.fi');
     await page.getByRole('button', { name: 'Kirjaudu', exact: true }).click();
     await page.frameLocator('internal:role=dialog[name="kirjaudu sisään"i] >> iframe').getByRole('button', { name: 'Luo Yle Tunnus' }).click();
     await page.frameLocator('internal:role=dialog[name="kirjaudu sisään"i] >> iframe').getByLabel('Sähköposti').click();
