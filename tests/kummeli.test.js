b06d7e71d15f13a8aeb26b7feb493d12cba5d911
emilalg <emil.algars@metropolia.fi>
2023-04-18 14:14:35 +0300

    Accessibility kummeli

--- tests/kummeli.test.js
+++ tests/kummeli.test.js
@@ -1,9 +1,15 @@
 import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
 
-test('Kummeli test', async ({ page }) => {
+test('Kummeli test', async ({ page }, testInfo) => {
   await page.goto('https://areena.yle.fi/1-3339547');
+
+  await axe(page, testInfo);
+
   await page.getByRole('button', { name: 'Kausi 3' }).click();
   await page.getByRole('link', { name: '5. Kummeli' }).click();
+
+  await axe(page, testInfo);
 
   //await page.getByRole('heading', { name: 'K3, J5: Kummeli' }).click();
   await expect(page.getByRole('heading', { name: 'K3, J5: Kummeli' })).toHaveText('K3, J5: Kummeli');
@@ -11,3 +17,20 @@ test('Kummeli test', async ({ page }) => {
   //await page.getByText('julkaistu ti 10.1.2006').click();
   await expect(page.getByText('julkaistu ti 10.1.2006')).toHaveText('julkaistu ti 10.1.2006');
 });
+
+async function axe(page, testInfo) {
+
+    try {
+      const accessibilityScanResults = await new AxeBuilder({ page })
+          .options({saveResult: true, resultPath: 'reports/axe-results.html' })
+          .analyze();
+      await testInfo.attach('accessibility-scan-results', {
+        body: JSON.stringify(accessibilityScanResults.violations, null, 2),
+        contentType: 'application/json'
+      });
+      
+    } catch(e) {
+      console.error(e.message);
+    }
+
+}
